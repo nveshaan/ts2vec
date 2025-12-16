b0f420f24270a9b93f94c4404b3e003a57084b24
nveshaan <nveshaan@gmail.com>
2025-12-16 18:06:47 +0530

    dit-s

---
+++ test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from ts2vec import TS2Vec
+
+device = 'mps'
+
+data = pd.read_csv('btcusd_ta.csv').to_numpy()[3668959:, 1:]
+data = data[: (len(data) // 300) * 300].reshape(-1, 300, data.shape[1])
+# Per-instance min-max normalization (scales each sample to [0, 1])
+min_vals = np.nanmin(data, axis=1, keepdims=True)
+max_vals = np.nanmax(data, axis=1, keepdims=True)
+range_vals = max_vals - min_vals
+range_vals[range_vals == 0] = 1  # Avoid division by zero
+data = (data - min_vals) / range_vals
+data = np.nan_to_num(data, nan=0.0, posinf=0.0, neginf=0.0)
+model = TS2Vec(
+    input_dims=data.shape[-1],
+    output_dims=384,
+    device=device,
+)
+model.load('training/btcusd_ta.csv__dit_s_20251216_153223/model.pkl')
+
+repr = model.encode(data, encoding_window='full_series')
+print('Representation shape:', repr.shape)
+
+plt.plot(repr.T, alpha=0.3)
+plt.title('TS2Vec Representations for BTC-USD Time Series Data')
+plt.tight_layout()
+plt.show()
